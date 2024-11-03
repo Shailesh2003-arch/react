bcd57d167a2bef772d12c5adec2a274bf5a59428
Shailesh2003-arch <117181669+Shailesh2003-arch@users.noreply.github.com>
2024-11-03 18:20:04 +0530

    folder added

---
+++ conditions/conditionalsinjsx/src/App.jsx
@@ -0,0 +1,50 @@
+
+function App() {
+
+  {/* here we are repeating the code to check the condition*/}
+  {/* if the age is >= 18 we will get a button that shows Available(/)}
+  {/* if the age is < 18 we will get a button that shows Not Available*/}
+
+  {/* Here we are basically repeating the code which is not a good practice */}
+
+  
+  {/*
+
+  let age = 18;
+  if (age>=18) {
+    return (
+      <>
+      <button >Available</button>
+      </>
+    )  
+  }
+
+  if (age<18) {
+    return (
+      <>
+      <button>Not Available</button>
+      </>
+    )  
+  }
+  
+  */}
+
+
+  {/* Using ternary operator */}
+
+let age = 14;
+    return (
+      <>
+      <button >{age>=18 ? "Available": "Not Available"}</button>
+      </>
+    )  
+
+}
+
+
+
+
+
+
+
+export default App
